3b101d72090d7443f336fda5663649899e513bb2
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-05-08 23:07:23 +0100

    task 10 & 11

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: Linked list to check
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head = NULL;
+
+	head = list;
+	while (list != NULL)
+	{
+		list = list->next;
+		if (head == list)
+			return (1);
+	}
+	return (0);
+}
