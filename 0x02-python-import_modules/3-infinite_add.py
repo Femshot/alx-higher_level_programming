3ff0f24bd31d8a454608c24f94f1d75711a21fae
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-05-11 21:19:18 +0100

    Correction to pycodestyle error in 3-infinte_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    l = []
+    list_num = []
     for c in argv:
-        l += [c]
+        list_num += [c]
     result = 0
-    for i in range(1, len(l)):
-        result += int(l[i])
+    for i in range(1, len(list_num)):
+        result += int(list_num[i])
     print("{:d}".format(result))
