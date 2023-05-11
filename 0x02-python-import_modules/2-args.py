1b34eb76f63ff33b5500c02c89f1d4e4d2bfbb74
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-05-11 21:14:27 +0100

    correction to 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    num = len(argv)
-    print("{:d} arguments:".format(num - 1))
-    l = []
+    num = len(argv) - 1
+    if num == 0:
+        print("{:d} arguments.".format(num))
+    elif num == 1:
+        print("{:d} argument:".format(num))
+    else:
+        print("{:d} arguments:".format(num))
+    list_args = []
     for c in argv:
-        l += [c]
-    for i in range(1, len(l)):
-        print("{}: {}".format(i, l[i]))
+        list_args += [c]
+    for i in range(1, len(list_args)):
+        print("{}: {}".format(i, list_args[i]))
