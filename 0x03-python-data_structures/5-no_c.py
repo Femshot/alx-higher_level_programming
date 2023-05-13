9aa0a0b265641b7a2b3f9e8dee38d59ac80e63ce
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-05-13 18:47:43 +0100

    removed module gaurd

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if __name__ == "__main__":
-        new_string = ""
-        for char in my_string:
-            if char != "c" and char != "C":
-                new_string += char
-        return new_string
+    new_string = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            new_string += char
+    return new_string
