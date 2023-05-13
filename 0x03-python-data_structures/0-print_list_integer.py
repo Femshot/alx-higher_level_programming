b6b0cae962b9005d38870c086676140391d9b818
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-05-13 18:10:37 +0100

    removed module gaurd

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if __name__ == "__main__":
-        for x in my_list:
-            print("{:d}".format(x))
+    for x in my_list:
+        print("{:d}".format(x))
