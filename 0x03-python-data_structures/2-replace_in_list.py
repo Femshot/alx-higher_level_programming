7f767691cf61a312742a3bc7cf08aad20accb023
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-05-13 18:17:54 +0100

    removed module gaurd codeline

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if __name__ == "__main__":
-        if (idx >= 0) and (idx < len(my_list)):
-            my_list[idx] = element
-        return my_list
+    if (idx >= 0) and (idx < len(my_list)):
+        my_list[idx] = element
+    return my_list
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/bin/bash/python3
 def new_in_list(my_list, idx, element):
-    if __name__ == "__main__":
-        new_list = my_list.copy()
-        if (idx >= 0) and (idx < len(my_list)):
-            new_list[idx] = element
-        return new_list
+    new_list = my_list.copy()
+    if (idx >= 0) and (idx < len(my_list)):
+        new_list[idx] = element
+    return new_list
