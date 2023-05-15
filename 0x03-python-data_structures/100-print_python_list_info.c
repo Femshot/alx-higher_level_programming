bf97dea93c93ed7b33da8b22d36bd58e53f56cb7
Femshot <awodelefemi@yahoo.com>
2023-05-15 22:26:10 +0000

    Finish documentation and run betty

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,16 +1,27 @@
 #include <Python.h>
 #include <stdio.h>
 
-/**
- */
+/
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t list_size = 0;
+	Py_ssize_t list_size = 0, i = 0;
+	PyObject *obj;
+	PyTypeObject *name;
+	PyListObject *list_alloc;
 
-	if (!(PyList_check(p)))
-		return 0;
+	if (!(PyList_Check(p)))
+		return;
 
 	list_size = PyList_Size(p);
+	list_alloc = (PyListObject *)p;
 
-	printf("[*] Size of the Python List = %d", (int)list_size);
+	printf("[*] Size of the Python List = %d\n", (int)list_size);
+	printf("[*] Allocated = %d\n", (int)list_alloc->allocated);
+	while (i < list_size)
+	{
+		obj = PyList_GetItem(p, i);
+		name = Py_TYPE(obj);
+		printf("Element %d: %s\n", (int)i, name->tp_name);
+		i++;
+	}
 }
