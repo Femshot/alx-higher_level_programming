5c5dfa98fc86eae2af5e5d4a7957c241b7d23f73
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-05-31 16:26:03 +0100

    Included Module level docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""Module containing an empty python class"""
 
 
 class Square:
-    """Empty class Square that defines a square.
-
-    Attributes:
-        empty
-
-    """
+    """Empty class Square that defines a square."""
     pass
