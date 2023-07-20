2d5cd58a2258d115d057c1947580006ac2615634
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-07-20 16:51:29 +0100

    include ifnot exists clause in qeuery

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,6 +5,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 -- Creates the table states in data base hbtn_od_usa
-CREATE TABLE states (
+CREATE TABLE IF NOT EXISTS states (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL);
