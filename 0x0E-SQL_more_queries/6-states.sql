77e1ccd0b9f7e7fb0ad9f674f4d6635714a7e8fd
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-07-20 16:36:59 +0100

    First  commit on tasks 2 - 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Creates the database hbtn_0d_usa 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+--Activate Database
+USE hbtn_0d_usa;
+
+-- Creates the table states in data base hbtn_od_usa
+CREATE TABLE states (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL);
