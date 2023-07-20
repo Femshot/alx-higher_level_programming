872834c0851b2cabb20de1717c09636d6bb45956
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-07-20 21:34:06 +0100

    Crrection to ordey by syntax

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lists all the cities of California that can be found in the database
 SELECT id, name FROM cities WHERE states_id = (
 	SELECT id FROM states WHERE name = 'California')
-	ORDER BY id;
+	ORDER BY cities.id;
