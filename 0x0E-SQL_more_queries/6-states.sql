1464e5e27a1bc18855d8c3612f308b9cacac1c17
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-07-20 17:01:19 +0100

    changed create command syntax for states table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,7 @@
 -- Creates the database hbtn_0d_usa 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
---Activate Database
-USE hbtn_0d_usa;
-
 -- Creates the table states in data base hbtn_od_usa
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
