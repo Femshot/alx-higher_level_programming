4eb62f4ac8a03ff653e92da4be5c92c29ba24d6c
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-08-19 12:08:02 +0100

    Corrected return module execution error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Lists all states from a mysql database passed as an argument"""
+""" Lists all states from a mysql database passed as an argument """
 import MySQLdb
 from sys import argv
 
 
-if __name__ != "__main__":
-    return
-
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                     password=argv[2], database=argv[3])
-db.query("SELECT id, name FROM states ORDER BY id")
-res = db.store_result()
-rows = res.fetch_row(maxrows=0, how=0)
-for row in rows:
-    print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], database=argv[3])
+    db.query("SELECT id, name FROM states ORDER BY id")
+    res = db.store_result()
+    rows = res.fetch_row(maxrows=0, how=0)
+    for row in rows:
+        print(row)
+    db.close()
