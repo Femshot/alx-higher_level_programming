1cc36c8d2b0f02300d728468cd83f53a582967bd
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-09-28 18:17:20 +0100

    added non execute on module import

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,11 +4,13 @@
 import requests as rq
 from sys import argv
 
-token = f"Bearer {argv[2]}"
-heads = {"Accept": "application/vnd.github+json",
-         "Authorization": token,
-         "X-GitHub-Api-Version": "2022-11-28"}
-url = "https://api.github.com/user"
 
-response = rq.get(url, headers=heads)
-print(response.json()['id'])
+if __name__ == "__main__":
+    token = f"Bearer {argv[2]}"
+    heads = {"Accept": "application/vnd.github+json",
+             "Authorization": token,
+            "X-GitHub-Api-Version": "2022-11-28"}
+    url = "https://api.github.com/user"
+
+    response = rq.get(url, headers=heads)
+    print(response.json()['id'])
