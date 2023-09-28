a347abbe95e38bb3f163ff72a722b66365768aaa
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-09-28 18:33:15 +0100

    adjusted import instruction

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,20 +4,20 @@ import requests
 from sys import argv
 
 
-if __name__ !="__main__":
-    exit()
-if len(argv) < 2:
-    pay = {'q': ""}
-else:
-    pay = {'q': argv[1]}
+if __name__ =="__main__":
 
-req = requests.post('http://0.0.0.0:5000/search_user', data=pay)
-if req.status_code == requests.codes.ok:
-    try:
-        code = req.json()
-        if code == {}:
-            print("No result")
-        else:
-            print(f'[{code["id"]}] {code["name"]}')
-    except Exception:
-        print("Not a valid JSON")
+    if len(argv) < 2:
+        pay = {'q': ""}
+    else:
+        pay = {'q': argv[1]}
+
+    req = requests.post('http://0.0.0.0:5000/search_user', data=pay)
+    if req.status_code == requests.codes.ok:
+        try:
+            code = req.json()
+            if code == {}:
+                print("No result")
+            else:
+                print(f'[{code["id"]}] {code["name"]}')
+        except Exception:
+            print("Not a valid JSON")
