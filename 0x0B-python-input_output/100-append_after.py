8bea6eb4c21f98af2577ad13c4f8a73713734ab0
Awodele Oluwafemi <awodelefemi@yahoo.com>
2024-07-25 16:37:07 +0100

    Correction to algorithm

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,9 +14,11 @@ def append_after(filename="", search_string="", new_string=""):
     """
     with open(filename, 'r') as target:
         line = target.readlines()
+        inserted_lines = 0
         for pos, string in enumerate(line.copy()):
             if string.find(search_string) != -1:
-                line.insert((pos + 1), new_string)
+                line.insert((pos + 1 + inserted_lines), new_string)
+                inserted_lines += 1
 
     with open(filename, 'w') as target:
         target.writelines(line)
